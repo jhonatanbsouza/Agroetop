e9b9204945f31a1737d662104c8b168db3128df3
parisi <160138803+parisilindona@users.noreply.github.com>
2024-07-05 16:45:21 -0300

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,21 @@
 const temaEscuro = document.querySelector("span")
+const cabecalho = document.querySelector(".cabecalho")
+const menuListaItem = document.querySelectorAll(".menu-lista-item")
 temaEscuro.onclick = function(){
     if (temaEscuro.textContent == "Escuro"){
         document.body.style.backgroundColor = "#282c27"
         temaEscuro.textContent = "Claro"
+        
+        menuListaItem[0].style.backgroundColor = "#282c27"
+        menuListaItem[1].style.backgroundColor = "#282c27"
+        menuListaItem[2].style.backgroundColor = "#282c27"
+        menuListaItem.style.color = "#FFFFFF"
+
+
     } else {
         document.body.style.backgroundColor = "#FFFFFF"
         temaEscuro.textContent = "Escuro"
+        menuListaItem.style.backgroundColor = "#FFFFFF"
     }
 }
 console.log(temaEscuro)
