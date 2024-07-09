712c683f912b548b4c29259f57201e2594b8830a
jhonatanbsouza <135228782+jhonatanbsouza@users.noreply.github.com>
2024-07-09 17:33:49 -0300

    Alterações 09-07
    Foram feitas as seguintes alterações:
    
    - Interação dos links com a alteração para o modo escuro/claro
    
    - Alteração do position do header para fixed 
    
    - Alteração de fonte

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,32 @@
-const temaEscuro = document.querySelector("span")
+const temaEscuro = document.querySelector(".tema-escuro")
 const cabecalho = document.querySelector(".cabecalho")
 const input = document.querySelector(".input-pesquisa")
 const usuario = document.querySelector(".usuario")
 const menuListaItem = document.querySelectorAll(".menu-lista-item")
+const link = document.querySelectorAll('a')
+const linkMenuLateral = document.querySelectorAll(".link-menu-lateral")
 temaEscuro.onclick = function(){
+
     if (temaEscuro.textContent == "Escuro"){
         document.body.style.backgroundColor = "#282c27"
         temaEscuro.textContent = "Claro"
-        
         menuListaItem[0].style.backgroundColor = "#282c27"
         menuListaItem[0].style.color = "#FFFFFF"
         menuListaItem[1].style.backgroundColor = "#282c27"
         menuListaItem[2].style.backgroundColor = "#282c27"
-
         input.style.backgroundColor = "#282c27"
         input.style.color = "#FFFFFF"
         usuario.style.backgroundColor = "#282c27"
         usuario.style.color = "#FFFFFF"
+
+        for (let i = 0 ; i < link.length ; i++){
+            link[i].style.color = '#FFFFFF'
+        }
+        for (let j = 0 ; j < linkMenuLateral.length ; j++){
+            linkMenuLateral[j].style.color = '#000000'
+        }
+
+
 
     } else {
         document.body.style.backgroundColor = "#FFFFFF"
@@ -31,6 +41,13 @@ temaEscuro.onclick = function(){
         input.style.color = "#000000"
         usuario.style.backgroundColor = "#FFFFFF"
         usuario.style.color = "#000000"
+        console.log(linkMenuLateral)
+        for (let i = 0 ; i < link.length ; i++){
+            link[i].style.color = '#000000'
+        }
+
+      
+    
     }
 }
 console.log(temaEscuro)
