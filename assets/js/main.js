f3490d9d7f4ff4cb2c8e59de554673a2853f539d
parisi <160138803+parisilindona@users.noreply.github.com>
2024-07-09 16:47:57 -0300

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,7 @@
 const temaEscuro = document.querySelector("span")
 const cabecalho = document.querySelector(".cabecalho")
+const input = document.querySelector(".input-pesquisa")
+const usuario = document.querySelector(".usuario")
 const menuListaItem = document.querySelectorAll(".menu-lista-item")
 temaEscuro.onclick = function(){
     if (temaEscuro.textContent == "Escuro"){
@@ -7,15 +9,28 @@ temaEscuro.onclick = function(){
         temaEscuro.textContent = "Claro"
         
         menuListaItem[0].style.backgroundColor = "#282c27"
+        menuListaItem[0].style.color = "#FFFFFF"
         menuListaItem[1].style.backgroundColor = "#282c27"
         menuListaItem[2].style.backgroundColor = "#282c27"
-        menuListaItem.style.color = "#FFFFFF"
 
+        input.style.backgroundColor = "#282c27"
+        input.style.color = "#FFFFFF"
+        usuario.style.backgroundColor = "#282c27"
+        usuario.style.color = "#FFFFFF"
 
     } else {
         document.body.style.backgroundColor = "#FFFFFF"
         temaEscuro.textContent = "Escuro"
-        menuListaItem.style.backgroundColor = "#FFFFFF"
+
+        
+        menuListaItem[0].style.backgroundColor = "#FFFFFF"
+        menuListaItem[1].style.backgroundColor = "#FFFFFF"
+        menuListaItem[2].style.backgroundColor = "#FFFFFF"
+        
+        input.style.backgroundColor = "#FFFFFF"
+        input.style.color = "#000000"
+        usuario.style.backgroundColor = "#FFFFFF"
+        usuario.style.color = "#000000"
     }
 }
 console.log(temaEscuro)
