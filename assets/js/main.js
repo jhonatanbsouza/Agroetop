0bacaabb6412b370376c7bd627bcbc025a92b762
parisi <160138803+parisilindona@users.noreply.github.com>
2024-08-03 12:26:30 -0300

    html novos
    html da minha conta e meus cursos. id do suporte e botoes de adquirir

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,13 +7,14 @@ const link = document.querySelectorAll('.link-menu-lista')
 const linkMenuLateral = document.querySelectorAll(".link-menu-lateral")
 const Main = document.querySelector('.main')
 const planosLink = document.querySelectorAll(".link-planos-lista")
+const textoConteudo = document.querySelectorAll(".texto-conteudo")
 
 
 temaEscuro.onclick = function(){
 
     if (temaEscuro.textContent == "Tema Escuro"){
         document.body.style.backgroundColor = "#282c27"
-        temaEscuro.textContent = "Tema Claro"
+        temaEscuro.textContent = "Tema Claro" 
         menuListaItem[0].style.backgroundColor = "#282c27"
         menuListaItem[0].style.color = "#FFFFFF"
         menuListaItem[1].style.backgroundColor = "#282c27"
@@ -22,6 +23,11 @@ temaEscuro.onclick = function(){
         input.style.color = "#FFFFFF"
         usuario.style.backgroundColor = "#282c27"
         usuario.style.color = "#FFFFFF"
+        textoConteudo[0].style.backgroundColor = "#31392f"
+        textoConteudo[1].style.backgroundColor = "#31392f"
+        textoConteudo[2].style.backgroundColor = "#31392f"
+        textoConteudo[3].style.backgroundColor = "#31392f"
+        
 
         Main.style.color = "#FFFFFF"
         
@@ -43,18 +49,22 @@ temaEscuro.onclick = function(){
 
 
     } else {
-        document.body.style.backgroundColor = "#FFFFFF"
+        document.body.style.backgroundColor = "#e8f8e6"
         temaEscuro.textContent = "Tema Escuro"
 
         
-        menuListaItem[0].style.backgroundColor = "#FFFFFF"
-        menuListaItem[1].style.backgroundColor = "#FFFFFF"
-        menuListaItem[2].style.backgroundColor = "#FFFFFF"
+        menuListaItem[0].style.backgroundColor = "#e8f8e6"
+        menuListaItem[1].style.backgroundColor = "#e8f8e6"
+        menuListaItem[2].style.backgroundColor = "#e8f8e6"
         
-        input.style.backgroundColor = "#FFFFFF"
+        input.style.backgroundColor = "#e8f8e6"
         input.style.color = "#000000"
-        usuario.style.backgroundColor = "#FFFFFF"
+        usuario.style.backgroundColor = "#e8f8e6"
         usuario.style.color = "#000000"
+        textoConteudo[0].style.backgroundColor = "#bcd1b7"
+        textoConteudo[1].style.backgroundColor = "#bcd1b7"
+        textoConteudo[2].style.backgroundColor = "#bcd1b7"
+        textoConteudo[3].style.backgroundColor = "#bcd1b7"
 
         Main.style.color = "#000000"
 
